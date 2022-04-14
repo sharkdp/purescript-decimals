08fb375a6ef110b9c96837576ecf653015e2fa69
mhmdanas <triallax@tutanota.com>
2022-04-14 17:12:30 +0300

    Add `DivisionRing` and `Field` instances for `Decimal`
    See
    https://github.com/sharkdp/purescript-decimals/issues/5#issuecomment-369847210.
    
    There's no need to explicitly define a `Field` instance:
    
    > This module also defines a single `Field` instance for any type which
    has both `EuclideanRing` and `DivisionRing` instances.
    
    Source:
    https://pursuit.purescript.org/packages/purescript-prelude/5.0.1/docs/Data.Field#t:Field

--- src/Data/Decimal.purs
+++ src/Data/Decimal.purs
@@ -135,6 +135,9 @@ instance EuclideanRing Decimal where
   mod _ _ = zero
   degree _ = one
 
+instance DivisionRing Decimal where
+  recip = dDiv one
+
 -- | Round to the given number of significant digits.
 foreign import toSignificantDigits ∷ Int → Decimal → Decimal
 
